929c9591fdac8afbff33df4cd1661cc21e2fa440
Mariko Medlock <mariko.medlock@gmail.com>
2020-02-12 17:34:36 -0500

    Make HTTP status request to ES cluster.

--- src/main/java/jadesearchpoc/Indexer.java
+++ src/main/java/jadesearchpoc/Indexer.java
@@ -1,11 +1,28 @@
 package jadesearchpoc;
 
+import jadesearchpoc.application.Config;
+import jadesearchpoc.utils.HTTPUtils;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Indexer {
 
 	// single-threaded version
-	public void indexSnapshot(String snapshot_id) {
+	public void indexSnapshot(String snapshot_id) throws Exception {
 
 		System.out.println("indexing snapshot: " + snapshot_id);
+
+		// check cluster status. the ip address here is to the cluster deployed in dev.
+		// curl -X GET "35.232.178.35:9200/_cluster/health?wait_for_status=yellow"
+		Map<String, String> httpParams = new HashMap<>();
+		httpParams.put("wait_for_status", "yellow");
+		Map<String, Object> httpResult = HTTPUtils.sendJavaHttpRequest(
+				Config.ElasticSearchIPAddress + ":" + Config.ElasticSearchPort + "/_cluster/health",
+				"GET",
+				httpParams);
+		System.out.println(httpResult);
 
 		// fetch elasticsearch highest root_row_id with this snapshot_id
 		// if none, set root_row_id to zero
--- src/main/java/jadesearchpoc/application/Config.java
+++ src/main/java/jadesearchpoc/application/Config.java
@@ -5,7 +5,7 @@ public class Config {
 	// e.g. ElasticSearch IP address and port, name of custom class to build the index document
 	// these parameters will likely be read from a file or fetched from a profile later
 
-	public static final String ElasticSearchIPAddress = "35.232.178.35";
+	public static final String ElasticSearchIPAddress = "http://35.232.178.35";
 	public static final String ElasticSearchPort = "9200";
 
 	public static final String DataRepoIPAddress = "https://jade-mm.datarepo-dev.broadinstitute.org";
