6f719260011d1557f5407b617e56a32985fbf1ea
Mariko Medlock <mariko.medlock@gmail.com>
2020-02-13 17:50:31 -0500

    Parse result of max root_row_id ES query.

--- src/main/java/jadesearchpoc/Indexer.java
+++ src/main/java/jadesearchpoc/Indexer.java
@@ -9,10 +9,14 @@ import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.ml.job.results.Bucket;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.Aggregations;
+import org.elasticsearch.search.aggregations.bucket.filter.Filter;
 import org.elasticsearch.search.aggregations.bucket.filter.FilterAggregationBuilder;
+import org.elasticsearch.search.aggregations.metrics.Max;
 import org.elasticsearch.search.aggregations.metrics.MaxAggregationBuilder;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.slf4j.Logger;
@@ -26,14 +30,15 @@ public class Indexer {
 		ElasticSearchUtils.checkClusterHealth();
 
 		// lookup snapshot id from name
-		SnapshotSummaryModel snapshotSummaryModel = DataRepoUtils.snapshotFromName(snapshotName);
-		if (snapshotSummaryModel == null) {
-			throw new Exception ("snapshot not found");
-		}
-		LOG.trace(DisplayUtils.prettyPrintJson(snapshotSummaryModel));
+//		SnapshotSummaryModel snapshotSummaryModel = DataRepoUtils.snapshotFromName(snapshotName);
+//		if (snapshotSummaryModel == null) {
+//			throw new Exception ("snapshot not found");
+//		}
+//		LOG.trace(DisplayUtils.prettyPrintJson(snapshotSummaryModel));
 
 		// call indexer with the snapshot id
-		indexSnapshot(snapshotSummaryModel.getId());
+		indexSnapshot(snapshotName);
+//		indexSnapshot(snapshotSummaryModel.getId());
 
 		// cleanup
 		APIPointers.closeElasticsearchApi();
@@ -48,41 +53,34 @@ public class Indexer {
 		SearchRequest searchRequest = new SearchRequest("testindex");
 		SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
 
+		// search for the largest root_row_id among all documents with a specific snapshot_id
+		//     - build the request object
 		QueryBuilder filterQuery = QueryBuilders.matchQuery("snapshot_id", snapshot_id);
 		FilterAggregationBuilder filter = AggregationBuilders.filter("snapshot_rows", filterQuery);
 		MaxAggregationBuilder max = AggregationBuilders.max("max_root_row_id").field("root_row_id");
 		filter.subAggregation(max);
 		searchSourceBuilder.aggregation(filter);
-
-//		TermsAggregationBuilder aggregation = AggregationBuilders.terms("by_company")
-//				.field("company.keyword");
-//		aggregation.subAggregation(AggregationBuilders.avg("average_age")
-//				.field("age"));
-//		searchSourceBuilder.aggregation(aggregation);
-
-//		searchSourceBuilder.query(QueryBuilders.matchAllQuery());
 		searchSourceBuilder.size(0);
 
+		//     - send the request
 		searchRequest.source(searchSourceBuilder);
 		SearchResponse searchResponse = esApi.search(searchRequest, RequestOptions.DEFAULT);
-		LOG.debug(DisplayUtils.prettyPrintJson(searchResponse));
+		LOG.trace(DisplayUtils.prettyPrintJson(searchResponse));
 
-	// search for the largest root_row_id among all documents with a specific snapshot_id
-	//		curl -X POST "35.232.178.35:9200/testindex/_search?size=0&pretty" -H 'Content-Type: application/json' -d'
-	//		{
-	//			"aggs" : {
-	//			"snapshot_rows" : {
-	//				"filter" : { "match": { "snapshot_id": "bb2ea099-d621-42b6-b2b3-faaa95b20849" } },
-	//				"aggs" : {
-	//					"max_root_row_id" : { "max" : { "field" : "root_row_id" } }
-	//				}
-	//			}
-	//		}
-	//		}
-	//		'
+		//     - parse the result object to find how many existing documents match the snapshot_id
+		Aggregations aggregations = searchResponse.getAggregations();
+		Filter snapshotRowsAgg = aggregations.get("snapshot_rows");
+		long numSnapshotRows = snapshotRowsAgg.getDocCount();
+		LOG.info("num docs found for snapshot: " + numSnapshotRows);
 
-		// fetch elasticsearch highest root_row_id with this snapshot_id
-		// if none, set root_row_id to zero
+		// if there are no existing documents, set root_row_id to zero
+		long rootRowId = 0;
+		if (numSnapshotRows > 0) {
+			Max maxRootRowIdAgg = snapshotRowsAgg.getAggregations().get("max_root_row_id");
+			double maxRootRowId = maxRootRowIdAgg.getValue();
+			LOG.debug("max_root_row_id found: " + maxRootRowId);
+			rootRowId = (long) maxRootRowId;
+		}
 
 		while (true) {
 			break;
