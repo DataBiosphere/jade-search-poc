5394750e8129dd2fa37c3070b2dd15a37a20e284
Mariko Medlock <mariko.medlock@gmail.com>
2020-02-18 17:03:14 -0500

    Add explanation comments to test script.

--- buildIndexDocument_test.sh
+++ buildIndexDocument_test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# This is an example script to build an index document, given the snapshot_id and root_row_id.
+# The snapshot_id and root_row_id are the first and second arguments, respectively, passed to the script.
+# Below is an example command to use this script to generate documents for each root_row_id in a snapshot.
+# poc index-snapshot 1000GenomesSnapshotA -t=sample_info -c=sample -i=testindex -b="sh buildIndexDocument_test.sh"
+
 # current date as yyyy-mm-dd HH:MM:SS
 now=$(date '+%Y-%m-%d %H:%M:%S')
 
